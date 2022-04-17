64544d4ba4f665526e436f461e40b75125cbe6ed
David Griffin <david.dtjg2001@gmail.com>
2022-04-17 16:36:59 -0700

    set up questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+var generateMarkdown = require('./utils/generateMarkdown');
+var fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: "What id the name of your project?",
+}, {
+    type: 'input',
+    name: 'description',
+    message: "What is your description?",
+},
+{
+    type: 'input',
+    name: 'table-of-contents',
+    message: "What is the table of contents?",
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: "How do we install the app?",
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: "What is the usage of the app?",
+},
+{
+    type: 'input',
+    name: 'credits',
+    message: "Who are the contributors of the project?",
+},
+{
+    type: 'list',
+    name: 'license',
+    message: "What is the license of the project?",
+    choices: ["GPL License", "MIT license", "Apache License 2.0", "BSD License", "Mozilla Public License 2.0", "None"],
+},
+{
+    type: 'input',
+    name: 'features',
+    message: "What are the features of the project?",
+},
+{
+    type: 'input',
+    name: 'contribute',
+    message: "How can people contribute to the project?",
+},
+{
+    type: 'input',
+    name: 'test',
+    message: "How can people test the project?",
+},
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, function (error, success) {
+        if (error) throw error;
+        console.log(fileName + "created successfully");
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // Use user feedback for... whatever!!
+            console.log(answers);
+            //convert into the readme friendly format 
+
+
+            //call the write file function to create e
+           // writeToFile("README.md", data);
+        })
+}
 
 // Function call to initialize app
 init();
